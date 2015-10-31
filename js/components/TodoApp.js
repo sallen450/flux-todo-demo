411fd90be303e012b4c561767c96136b6b447d69
jiqinghua <jiqinghua@souche.com>
2015-10-31 20:48:57 +0800

    add react main component

---
+++ js/components/TodoApp.js
@@ -0,0 +1,42 @@
+var Footer = require('./Footer');
+var Header = require('./Header');
+var MainSection = require('./MainSection');
+var React = require('react');
+var TodoStore = require('../stores/TodoStore');
+
+/*
+ * retrieve data from TodoStore
+ */
+
+function getTodoState() {
+    return {
+        allTodos: TodoStore.getAll();
+    }
+}
+
+function _onChange() {
+    this.setState(getTodoState());
+};
+
+var TodoApp = React.createClass({
+    getInitState: function() {
+        return getTodoState();
+    },
+
+    componentDidMount: function() {
+        TodoStore.addChangeListener(this._onChange);
+    },
+
+    componentWillUnmount: function() {
+        TodoStore.removeChangeListener(this._onChange);
+    },
+
+    render: function() {
+
+        return (
+            <Header />
+            <MainSection allTodos={this.state.allTodos} areAllComplete={this.state.areAllComplete} />
+            <Footer allTodos={this.state.allTodos} />
+        );
+    }
+});
