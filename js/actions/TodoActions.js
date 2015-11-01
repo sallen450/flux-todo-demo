dcf7a9414bccbb4aca9e336e36f7fdafcfc18fe9
jiqinghua <jiqinghua@souche.com>
2015-11-01 21:00:26 +0800

    add action

--- js/actions/TodoActions.js
+++ js/actions/TodoActions.js
@@ -1,5 +1,5 @@
 var AppDispatcher = require('../dispatcher/AppDispatcher');
-var TodoConstants = require('../constants/TodoConstants');
+var a = require('../constants/TodoConstants');
 
 var TodoActions = {
     create: function(text) {
